83c83b6be58cb294776fe9d23f06cba7084d010c
Cristi Constantin <cristi.constantin@Live.com>
2012-03-06 19:18:04 +0200

    Quite a few changes. Removed visual editor. That can be done with Scrambled-Egg. The password generated is now 24 characters. The password is created on key press, instead of line change.

--- bottle_generator.py
+++ bottle_generator.py
@@ -8,17 +8,17 @@
 
 import os, sys
 import platform
+import webbrowser
+
 import json
 import binascii as ba
 import cStringIO as IO
-
-from segg import ScrambledEgg
 from pbkdf2 import PBKDF2
 
-from bottle import default_app, run, route, get, post, debug
+from bottle import default_app, run, route, post, debug
 from bottle import response, request, template, static_file
 
-BASE_PATH = os.path.dirname(__file__)
+BASE_PATH = os.path.dirname(__file__) or os.getcwd()
 
 SITE = ''
 PWD = ''
@@ -35,11 +35,12 @@ def server_static(filename=None, what=None):
 def home():
     return template(BASE_PATH + '/template.htm')
 
+# Text site name
 @post('/jqXHR/s')
 def ajax_call_s():
     #
-    if not request.header.get('X-Requested-With') == 'XMLHttpRequest':
-        return
+    if not request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        return 'Invalid XMLHttpRequest!'
     global SITE
     SITE = request.forms.get('val', '')
     resp = {'message': generatePassword()}
@@ -47,11 +48,12 @@ def ajax_call_s():
     return json.dumps(resp)
     #
 
+# Text password
 @post('/jqXHR/p')
 def ajax_call_p():
     #
-    if not request.header.get('X-Requested-With') == 'XMLHttpRequest':
-        return
+    if not request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        return 'Invalid XMLHttpRequest!'
     global PWD
     PWD = request.forms.get('val', '')
     resp = {'message': generatePassword()}
@@ -59,61 +61,44 @@ def ajax_call_p():
     return json.dumps(resp)
     #
 
-# Grafical password.
+# Grafical password
 @post('/jqXHR/g')
 def ajax_call_g():
     #
-    if not request.header.get('X-Requested-With') == 'XMLHttpRequest':
-        return
+    if not request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        return 'Invalid XMLHttpRequest!'
     global SITE, PWD
     colors = request.forms.get('val', '')
     PWD = ' '.join(['0' if x == 'ffffff' else x for x in colors.split()])
     SITE = ' '.join(['0' if x != 'ffffff' else 'fff' for x in colors.split()])
-    resp = {'message': generatePassword(25)}
+    resp = {'message': generatePassword(24)}
     response.content_type = 'application/json; charset=UTF-8'
     return json.dumps(resp)
     #
 
-@post('/jqXHR/enc')
-def ajax_call_enc():
-    #
-    if not request.header.get('X-Requested-With') == 'XMLHttpRequest':
-        return
-    #
-    segg = ScrambledEgg()
-    step1 = request.forms.get('1', '').upper()
-    step2 = request.forms.get('2', '')
-    step3 = request.forms.get('3', '')
-    INPUT = request.forms.get('val', '')
-    #
-    # String...
-    #OUTPUT = segg.encrypt(txt=INPUT, pre=step1, enc=step2, post=step3, pwd='0', tags=False)
-    #
-    # Image...
-    OUTPUT = IO.StringIO()
-    segg.toImage(txt=INPUT, pre=step1, enc=step2, post=step3, pwd='0', path=OUTPUT)
-    IMG = ba.b2a_base64(OUTPUT.getvalue())
-    #
-    resp = {'message': 'data:image/png;base64,' + IMG}
-    response.content_type = 'application/json; charset=UTF-8'
-    return json.dumps(resp)
-    #
-
+# Helper function
 def generatePassword(size=0):
     global SITE, PWD
+    SITE = SITE.strip()
+    PWD = PWD.strip()
+    if not SITE or not PWD: return ''
     if not size:
         size = len(PWD)
-    if not SITE or not PWD: return ''
+    if size > 24:
+        size = 24
     txt = PBKDF2(passphrase=PWD, salt=SITE, iterations=1024).read(size*2)
     return ba.b2a_base64(txt)[:size]
 
-#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-if platform.system().lower()=='windows':
-    debug(True)
-    run(host='localhost', port=333, reloader=True)
-else:
-    path = '/home/croqqq/pwd_gen'
-    if path not in sys.path:
-        sys.path.append(path)
-    application = default_app()
+if __name__ == '__main__':
+
+    if platform.system().lower()=='windows':
+        debug(True)
+        webbrowser.open_new_tab('http://localhost:333/')
+        run(host='localhost', port=333, reloader=False)
+    else:
+        path = '/home/croqqq/pwd_gen'
+        if path not in sys.path:
+            sys.path.append(path)
+        application = default_app()
